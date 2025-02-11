511afd729f5d95df2565c4939abe3ef48f37e5f6
André Wallat <andre.wallat@wemove.com>
2025-02-11 12:22:48 +0100

    Update libs to fix security issues (#7326)

--- src/main/java/de/ingrid/iplug/dsc/webapp/controller/BlpImportController.java
+++ src/main/java/de/ingrid/iplug/dsc/webapp/controller/BlpImportController.java
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.2 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * https://joinup.ec.europa.eu/software/page/eupl
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,9 +28,8 @@ import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.Collection;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -68,7 +67,7 @@ public class BlpImportController extends AbstractController {
 
     @RequestMapping(value = { "/iplug-pages/welcome.html", "/iplug-pages/excelUpload.html" }, method = RequestMethod.GET)
     public String showBlpImport(@ModelAttribute("uploadBean") final UploadBean uploadBean, final ModelMap modelMap, HttpServletRequest request, HttpServletResponse response,
-            @ModelAttribute("plugDescription") final PlugdescriptionCommandObject commandObject) throws Exception {
+                                @ModelAttribute("plugDescription") final PlugdescriptionCommandObject commandObject) throws Exception {
         return AdminViews.EXCEL_UPLOAD;
     }
 
@@ -86,7 +85,7 @@ public class BlpImportController extends AbstractController {
      */
     @RequestMapping(value = {"/iplug-pages/excelUpload.html"}, method = RequestMethod.POST)
     public String upload(
-            @ModelAttribute("uploadBean") final UploadBean uploadBean, 
+            @ModelAttribute("uploadBean") final UploadBean uploadBean,
             @ModelAttribute("plugDescription") final PlugdescriptionCommandObject commandObject)
             throws IOException {
 
